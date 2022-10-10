d29cb53103f9871baa3e4e37e08b0c75dd6954d9
Buyole Isacko <isackobuyole@gmail.com>
2022-10-10 12:50:28 +0300

    xml implemented

--- app/src/main/java/com/example/cropimage/MainActivity.kt
+++ app/src/main/java/com/example/cropimage/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.cropimage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    private val cropActivityResultContract = object: ActivityResultContract<Any?>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
