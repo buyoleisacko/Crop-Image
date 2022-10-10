498a62ddac139cdb9f466ac8028b16622ae4a49b
Buyole Isacko <isackobuyole@gmail.com>
2022-10-10 15:26:18 +0300

    crop image dependency added to android manifest

--- app/src/main/java/com/example/cropimage/MainActivity.kt
+++ app/src/main/java/com/example/cropimage/MainActivity.kt
@@ -1,18 +1,46 @@
 package com.example.cropimage
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private val cropActivityResultContract = object: ActivityResultContract<Any?>
+    private val cropActivityResultContract = object: ActivityResultContract<Any?, Uri?>(){
+        override fun createIntent(context: Context, input: Any?): Intent {
+            return CropImage.activity()
+                .setAspectRatio(16, 19)
+                .getIntent(this)
+        }
+
+        override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
+            return CropImage.getActivityResult(intent)?.uri
+        }
+    }
+
+    private lateinit var cropActivityResultLauncher: ActivityResultLauncher<Any?>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnChooseImage = findViewById<Button>(R.id.btnChooseImage)
+        val ivCroppedImage = findViewById<ImageView>(R.id.ivCroppedImage)
+        cropActivityResultLauncher  = registerForActivityResult(cropActivityResultContract){
+            it?.let { uri ->
+                ivCroppedImage.setImageURI(uri)
+            }
+        }
+        btnChooseImage.setOnClickListener {
+            cropActivityResultLauncher.launch(null)
+        }
     }
 
 }
